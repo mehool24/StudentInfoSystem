e9629a55a35e363f82b70bbf7e69895133b83d66
Xkushal <kushalyhakuri123@gmail.com>
2020-07-09 14:52:01 -0500

    created one to one relation with Address

--- src/main/java/com/vastika/smd/model/CollegeInfo.java
+++ src/main/java/com/vastika/smd/model/CollegeInfo.java
@@ -1,5 +1,67 @@
 package com.vastika.smd.model;
 
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="college_info")
 public class CollegeInfo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "college_name")
+	private String collegeName;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+	@OneToOne
+	private Student student;
+	
+	public CollegeInfo() {
+		
+	}
+	
+	public CollegeInfo(int id, String collegeName, Address address, Student student) {
+		super();
+		this.id = id;
+		this.collegeName = collegeName;
+		this.address = address;
+		this.student = student;
+	}
+	
+	
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 }
