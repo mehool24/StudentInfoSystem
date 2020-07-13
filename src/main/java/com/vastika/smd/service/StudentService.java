11f82208f6695f9bb74479bdcf120e1677b4b86a
Xkushal <kushalyhakuri123@gmail.com>
2020-07-12 22:32:01 -0500

    new method for sending confirmation email is created

--- src/main/java/com/vastika/smd/service/StudentService.java
+++ src/main/java/com/vastika/smd/service/StudentService.java
@@ -2,6 +2,8 @@ package com.vastika.smd.service;
 
 import java.util.List;
 
+import org.springframework.mail.SimpleMailMessage;
+
 import com.vastika.smd.model.Student;
 
 public interface StudentService {
@@ -11,5 +13,11 @@ public interface StudentService {
 	Student getStudentByUserNameAndpassword(String username, String password);
 
 	void resetPassword(Student student);
+	
+	Student getStudentByEmail(String email);
+	
+	void updateStudentInfo(Student student);
+	
+	void sendMail(SimpleMailMessage simpleMessage);
 
 }
